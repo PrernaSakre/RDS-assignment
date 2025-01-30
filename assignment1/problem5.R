99f11cfe334c1481cddf38cca36d40b5c790e79b
PrernaSakre <165878207+PrernaSakre@users.noreply.github.com>
2025-01-30 18:05:42 +0530

    Update problem5.R

--- assignment1/problem5.R
+++ assignment1/problem5.R
@@ -1,3 +1,6 @@
-result<-c(vector1,vector2,vector3,vector4)
-print(length(result)==20)
-[1] TRUE
+vec <- rep(c(-1, 3, -5, 7, -9), each=10, times=2)
+vec <- c(6:12, rep(5.3, 3), -3, seq(102, length(vec), length.out=9))
+print(length(vec)==20)
+
+#output:
+#[1] TRUE
