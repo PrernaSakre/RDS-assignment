ae724c9090fd8a889cd419c28fa633f6884006f6
PrernaSakre <165878207+PrernaSakre@users.noreply.github.com>
2025-01-30 13:37:31 +0530

    Update problem4.R

--- assignment1/problem4.R
+++ assignment1/problem4.R
@@ -1,8 +1,8 @@
-x<-seq(from=6,to=12,by=1)
-y<-rep(5.3,times=3)
-z<--3
-w<-seq(from=102,to=length(c),length.out=9)
-e<-c(x,y,z,w)
-e
+vector1<-seq(from=6,to=12,by=1)
+vector2<-rep(5.3,times=3)
+vector3<--3
+vector4<-seq(from=102,to=length(c),length.out=9)
+result<-c(x,y,z,w)
+result
  [1]   6.00   7.00   8.00   9.00  10.00  11.00  12.00   5.30   5.30   5.30  -3.00
 [12] 102.00 101.75 101.50 101.25 101.00 100.75 100.50 100.25 100.00
